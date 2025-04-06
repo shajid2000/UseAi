cad831647d82d63de9dd529c770af288ce0d6349
shajid <shajid@gmail.com>
2025-04-06 23:10:03 +0530

    setting allow host

--- UseAi/settings.py
+++ UseAi/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-07-sap5(0r8x&x2d5a=7gx7&5vx_8kgauuftzh_*f=k=ckpy_7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['useai.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -133,3 +133,4 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 ASGI_APPLICATION = 'AroBot.asgi.application'
+
